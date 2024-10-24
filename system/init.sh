b301498cf010b12618ff0fe3bd4f17491c54c250
Dmitry Zhuravlev <dpzhuravlev@corp.finam.ru>
2024-10-24 12:42:46 +0300

    fix nginx

--- system/init.sh
+++ system/init.sh
@@ -1,6 +1,10 @@
 sudo apt update
 sudo apt upgrade -y
 
+echo "Config git"
+git config core.fileMode false
+git config pull.rebase true
+
 echo "Install dependencies Docker"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
