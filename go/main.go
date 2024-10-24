7939def491249fc59b5bd0f279551ad229c9003b
Destroyer86 <destroyer.86@mail.ru>
2024-10-24 17:57:59 +0300

    [*] add WriteToDB

--- go/main.go
+++ go/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-    "context"
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "github.com/jackc/pgx/v4"
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    r.GET("/api/health", func(c *gin.Context) {
-        conn, err := pgx.Connect(context.Background(), "postgresql://hackathon:hackathon@postgresql:5432/hackathon")
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"status": "Database is not accessible", "error": err.Error()})
-            return
-        }
-        defer conn.Close(context.Background())
+	r.GET("/api/health", func(c *gin.Context) {
+		conn, err := pgx.Connect(context.Background(), "postgresql://hackathon:hackathon@postgresql:5432/hackathon")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "Database is not accessible", "error": err.Error()})
+			return
+		}
+		defer conn.Close(context.Background())
 
-        c.JSON(http.StatusOK, gin.H{"status": "Database is accessible"})
-    })
+		c.JSON(http.StatusOK, gin.H{"status": "Database is accessible"})
+	})
 
-    r.Run(":8080")
+	r.Run(":8080")
 }
