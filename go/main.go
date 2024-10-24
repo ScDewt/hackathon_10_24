fe7b024ca91b583bbd5773a1d8948b707d340e0a
Дмитрий Глижинский <daemon191@gmail.com>
2024-10-24 17:44:04 +0300

    add source manager

--- go/main.go
+++ go/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-    "context"
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "github.com/jackc/pgx/v4"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
+	"healthcheck/source_manager"
+	"net/http"
+	"os"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    r.GET("/api/health", func(c *gin.Context) {
-        conn, err := pgx.Connect(context.Background(), "postgresql://hackathon:hackathon@postgresql:5432/hackathon")
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"status": "Database is not accessible", "error": err.Error()})
-            return
-        }
-        defer conn.Close(context.Background())
+	dsn := getEnv("DSN", "postgresql://hackathon:hackathon@postgresql:5432/hackathon")
 
-        c.JSON(http.StatusOK, gin.H{"status": "Database is accessible"})
-    })
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
 
-    r.Run(":8080")
+	r.GET("/api/health", func(c *gin.Context) {
+		_, err := conn.Exec(c.Request.Context(), "SELECT 1")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "Database is not accessible", "error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "Database is accessible"})
+	})
+
+	source_manager.NewSourceManagerHandler(conn).RegisterHandler(r)
+
+	r.Run(":8080")
+}
+
+// getEnv получает строку из переменных окружения или возвращает значение по умолчанию
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }
